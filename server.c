22f145b04ba290a94d46b9da07dff1583d311068
Charles Zhang <charleszhang2000@gmail.com>
2017-01-10 17:33:54 -0500

    lemao

--- server.c
+++ server.c
@@ -38,17 +38,18 @@ int main()
  }
 
   printf("found connection from [%d]\n",getpid());
-  while(1){
-      char buffer[256];
-      read(newsockdes,buffer,sizeof(buffer));
+  char buffer[256];
+  while(read(newsockdes,buffer,sizeof(buffer))){
+    //      char buffer[256];
+    //      read(newsockdes,buffer,sizeof(buffer));
       /*if(!strcmp(buffer,"exit\n"))
 	{
 	  close(newsockdes);
-	  printf("[%d] disconnected\n",getpid());
+	  
 	  return 0;
 	  }*/
       printf("message from [%d]: %s",getpid(),buffer);
      }
-  
+  printf("[%d] disconnected\n",getpid());
   return 0;
 }
